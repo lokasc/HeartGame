f0dbcc1a121268c4774e2a28428bd7029e4b017a
Matthew Lamm <matthewjlamm@gmail.com>
2026-05-18 16:03:46 -0400

    Started body manager

--- Assets/Scripts/Managers/BodyManager.cs
+++ Assets/Scripts/Managers/BodyManager.cs
@@ -1,8 +1,12 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class BodyManager : MonoBehaviour
 {
     public static BodyManager Instance { get; private set; }
+
+    [SerializeField] private List<TraitSO> bodyTraits;
 
     private void Awake()
     {
@@ -13,16 +17,59 @@ public class BodyManager : MonoBehaviour
         }
         Instance = this;
     }
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    private void OnEnable()
     {
-        
+        Patient.OnPatientTraitsLoaded += HandlePatientTraitsLoaded;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        Patient.OnPatientTraitsLoaded -= HandlePatientTraitsLoaded;
+    }
+
+
+    private void HandlePatientTraitsLoaded(string patientName)
+    {
+        chartTraits = TraitManager.Instance.GetTraitsByRepresentedOn("chart");
+
+        foreach (TraitSO trait in chartTraits)
+        {
+            Debug.Log($"Chart trait found: {trait.traitName} with data: {trait.traitData}");
+        }
+
+
+        ImportChartTrait("fullName", nameSlot, out TraitSO fullNameTrait);
+        this.fullNameTrait = fullNameTrait;
+        ImportChartTrait("age", ageSlot, out TraitSO ageTrait);
+        this.ageTrait = ageTrait;
+        ImportChartTrait("dob", dobSlot, out TraitSO dobTrait);
+        this.dobTrait = dobTrait;
+        ImportChartTrait("sexatbirth", sexSlot, out TraitSO sexTrait);
+        this.sexTrait = sexTrait;
+        ImportChartTrait("pronouns", pronounSlot, out TraitSO pronounsTrait);
+        this.pronounsTrait = pronounsTrait;
+        ImportChartTrait("weight", weightSlot, out TraitSO weightTrait);
+        this.weightTrait = weightTrait;
+        ImportChartTrait("race", raceSlot, out TraitSO raceTrait);
+        this.raceTrait = raceTrait;
+        ImportChartTrait("temperature", tempSlot, out TraitSO tempTrait);
+        this.tempTrait = tempTrait;
+        ImportChartTrait("o2", o2Slot, out TraitSO o2Trait);
+        this.o2Trait = o2Trait;
+        ImportChartTrait("bp", bpSlot, out TraitSO bpTrait);
+        this.bpTrait = bpTrait;
+        ImportChartTrait("rr", rrSlot, out TraitSO rrTrait);
+        this.rrTrait = rrTrait;
+        ImportChartTrait("pain", painSlot, out TraitSO painTrait);
+        this.painTrait = painTrait;
+        ImportChartTrait("edema", edemaSlot, out TraitSO edemaTrait);
+        this.edemaTrait = edemaTrait;
+        ImportChartTrait("ef", efSlot, out TraitSO efTrait);
+        this.efTrait = efTrait;
+        ImportChartTrait("hr", hrSlot, out TraitSO hrTrait);
+        this.hrTrait = hrTrait;
+
+        UpdatePanelNameSlots();
     }
 }
