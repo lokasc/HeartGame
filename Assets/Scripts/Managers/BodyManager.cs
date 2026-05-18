f372157f63eba361d3431d4275eb578aafb8bff6
Matthew Lamm <matthewjlamm@gmail.com>
2026-05-18 13:46:42 -0400

    Fix notes panel

---
+++ Assets/Scripts/Managers/BodyManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class BodyManager : MonoBehaviour
+{
+    public static BodyManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+    }
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
