712062d3e938f9e1a9f5d0ee97420ff9df3adbd5
Matthew Lamm <matthewjlamm@gmail.com>
2026-05-19 11:35:39 -0400

    Body manager begin

--- Assets/Scripts/Managers/BodyManager.cs
+++ Assets/Scripts/Managers/BodyManager.cs
@@ -31,45 +31,45 @@ public class BodyManager : MonoBehaviour
 
     private void HandlePatientTraitsLoaded(string patientName)
     {
-        chartTraits = TraitManager.Instance.GetTraitsByRepresentedOn("chart");
+        bodyTraits = TraitManager.Instance.GetTraitsByRepresentedOn("body");
 
-        foreach (TraitSO trait in chartTraits)
+        foreach (TraitSO trait in bodyTraits)
         {
-            Debug.Log($"Chart trait found: {trait.traitName} with data: {trait.traitData}");
+            Debug.Log($"Body trait found: {trait.traitName} with data: {trait.traitData}");
         }
 
 
-        ImportChartTrait("fullName", nameSlot, out TraitSO fullNameTrait);
-        this.fullNameTrait = fullNameTrait;
-        ImportChartTrait("age", ageSlot, out TraitSO ageTrait);
-        this.ageTrait = ageTrait;
-        ImportChartTrait("dob", dobSlot, out TraitSO dobTrait);
-        this.dobTrait = dobTrait;
-        ImportChartTrait("sexatbirth", sexSlot, out TraitSO sexTrait);
-        this.sexTrait = sexTrait;
-        ImportChartTrait("pronouns", pronounSlot, out TraitSO pronounsTrait);
-        this.pronounsTrait = pronounsTrait;
-        ImportChartTrait("weight", weightSlot, out TraitSO weightTrait);
-        this.weightTrait = weightTrait;
-        ImportChartTrait("race", raceSlot, out TraitSO raceTrait);
-        this.raceTrait = raceTrait;
-        ImportChartTrait("temperature", tempSlot, out TraitSO tempTrait);
-        this.tempTrait = tempTrait;
-        ImportChartTrait("o2", o2Slot, out TraitSO o2Trait);
-        this.o2Trait = o2Trait;
-        ImportChartTrait("bp", bpSlot, out TraitSO bpTrait);
-        this.bpTrait = bpTrait;
-        ImportChartTrait("rr", rrSlot, out TraitSO rrTrait);
-        this.rrTrait = rrTrait;
-        ImportChartTrait("pain", painSlot, out TraitSO painTrait);
-        this.painTrait = painTrait;
-        ImportChartTrait("edema", edemaSlot, out TraitSO edemaTrait);
-        this.edemaTrait = edemaTrait;
-        ImportChartTrait("ef", efSlot, out TraitSO efTrait);
-        this.efTrait = efTrait;
-        ImportChartTrait("hr", hrSlot, out TraitSO hrTrait);
-        this.hrTrait = hrTrait;
+        // ImportChartTrait("fullName", nameSlot, out TraitSO fullNameTrait);
+        // this.fullNameTrait = fullNameTrait;
+        // ImportChartTrait("age", ageSlot, out TraitSO ageTrait);
+        // this.ageTrait = ageTrait;
+        // ImportChartTrait("dob", dobSlot, out TraitSO dobTrait);
+        // this.dobTrait = dobTrait;
+        // ImportChartTrait("sexatbirth", sexSlot, out TraitSO sexTrait);
+        // this.sexTrait = sexTrait;
+        // ImportChartTrait("pronouns", pronounSlot, out TraitSO pronounsTrait);
+        // this.pronounsTrait = pronounsTrait;
+        // ImportChartTrait("weight", weightSlot, out TraitSO weightTrait);
+        // this.weightTrait = weightTrait;
+        // ImportChartTrait("race", raceSlot, out TraitSO raceTrait);
+        // this.raceTrait = raceTrait;
+        // ImportChartTrait("temperature", tempSlot, out TraitSO tempTrait);
+        // this.tempTrait = tempTrait;
+        // ImportChartTrait("o2", o2Slot, out TraitSO o2Trait);
+        // this.o2Trait = o2Trait;
+        // ImportChartTrait("bp", bpSlot, out TraitSO bpTrait);
+        // this.bpTrait = bpTrait;
+        // ImportChartTrait("rr", rrSlot, out TraitSO rrTrait);
+        // this.rrTrait = rrTrait;
+        // ImportChartTrait("pain", painSlot, out TraitSO painTrait);
+        // this.painTrait = painTrait;
+        // ImportChartTrait("edema", edemaSlot, out TraitSO edemaTrait);
+        // this.edemaTrait = edemaTrait;
+        // ImportChartTrait("ef", efSlot, out TraitSO efTrait);
+        // this.efTrait = efTrait;
+        // ImportChartTrait("hr", hrSlot, out TraitSO hrTrait);
+        // this.hrTrait = hrTrait;
 
-        UpdatePanelNameSlots();
+        // UpdatePanelNameSlots();
     }
 }
